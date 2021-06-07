008e419181f8deed5b00dce5bc1efeb22ac7bff8
Trey <trey.husko@gmail.com>
2021-06-06 19:50:55 -0700

    Added 4 caculator functions

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""
+calculator app using CI
+""" 
+
+def add(a,b):
+    return a + b
+def subtract(a,b):
+    return a - b
+def multiply(a,b):
+    return a * b
+def divide(a,b):
+    return a / b 
